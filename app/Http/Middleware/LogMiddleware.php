72fa176d9f8895830f4e5a22d786607bc907488e
Egor <egor.corostin@gmail.com>
2023-09-21 11:38:33 +0400

    add log middleware

---
+++ app/Http/Middleware/LogMiddleware.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class LogMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        info($request->ip(), ["all" => $request->url(), "body" => $request->all()]);
+        return $next($request);
+    }
+}
