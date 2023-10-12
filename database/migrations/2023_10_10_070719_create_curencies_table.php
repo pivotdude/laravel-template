7b796ffb969f7cb64ba01ef5c58a3b123cc9fdad
Egor <egor.corostin@gmail.com>
2023-10-12 10:23:01 +0400

    currency

---
+++ database/migrations/2023_10_10_070719_create_curencies_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('curencies', function (Blueprint $table) {
+            $table->id();
+
+            $table->string("name");
+            $table->decimal("price", 8, 2)->unsigned();
+            $table->boolean("active")->default(true);
+            $table->integer("sorting")->unsigned()->default(0);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('curencies');
+    }
+};
