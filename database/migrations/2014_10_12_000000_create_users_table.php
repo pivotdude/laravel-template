03ff7f22a7dad8a1c9842e947e605461ffd961ec
Egor <egor.corostin@gmail.com>
2024-01-13 10:00:40 +0400

    added Post model, user model

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,12 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('login')->unique();
             $table->string('email')->unique();
+            $table->string('phone');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean("isAdmin")->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
